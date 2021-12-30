eaa78a7d5b7ead15432b87d529b2e3bca0ef64f3
fernandobarbosa <fernandobsjunior@gmail.com>
2021-12-30 14:41:17 -0300

    Initial commit.

---
+++ app/src/main/java/br/com/fernandobarbosa/jetnote/MainActivity.kt
@@ -0,0 +1,24 @@
+package br.com.fernandobarbosa.jetnote
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import br.com.fernandobarbosa.jetnote.ui.theme.JetNoteTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetNoteTheme {
+                Surface(color = MaterialTheme.colors.background) {
+
+                }
+            }
+        }
+    }
+}
